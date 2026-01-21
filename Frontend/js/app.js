545205a9b3a7c70595b8d2cf676522b45756ec5f
Leonardo Barreiro Costellini <leonardobarreiro26@gmail.com>
2026-01-21 08:13:25 -0300

    Add files via upload

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -3,7 +3,7 @@ const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
 
 
-const res = await fetch('/api/routes/auth', {
+const res = await fetch('/api/auth', {
 method: 'POST',
 headers: { 'Content-Type': 'application/json' },
 body: JSON.stringify({ email, password })
